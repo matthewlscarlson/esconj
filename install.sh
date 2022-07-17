1aa146759428e938e92300d89a561cda99f2cac9
Matthew Carlson <matt@mcarlson.xyz>
2022-07-17 03:16:54 -0400

    made some slight improvements

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-[ "$EUID" -ne 0 ] && echo "Permission denied. Are you root?" && exit
+[ "$(id -u)" -ne 0 ] && echo 'Permission denied. Are you root?' && exit 1
 
-DIR='/usr/share/esconj'
+DIR='/usr/share/esconj/'
 PATH='/usr/bin/'
 
-mkdir --parents "${DIR}"
-echo "./verbs.csv -> ${DIR}/verbs.csv" && cp './verbs.csv' "${DIR}/verbs.csv"
-echo "./esconj -> ${PATH}/esconj" && cp './esconj' "${PATH}/esconj"
+mkdir -p "${DIR}"
+echo "./verbs.csv -> ${DIR}/verbs.csv"
+cp './verbs.csv' "${DIR}/verbs.csv"
+echo "./esconj -> ${PATH}/esconj"
+cp './esconj' "${PATH}/esconj"
 
-[ $? -eq 0 ] && echo "Installation complete."
+echo 'Installation complete.'
